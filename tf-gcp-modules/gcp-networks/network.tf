c4a7171259aa1b4a9f3a19f6b2fffdd1554c2b10
evert0nl <everton.luiz82@gmail.com>
2021-12-01 21:46:22 -0300

    versao

---
+++ tf-gcp-modules/gcp-networks/network.tf
@@ -0,0 +1,9 @@
+
+  module  "network" {
+      source = "git@github.com:evert0nl/tf-gcp-modules.git?ref=v1.0"
+      name = "ex aula7"
+      description = "Aula07"
+      project = "aula"
+
+  }
+
--- tf-gcp-modules/gcp-networks/outputs.tf
+++ tf-gcp-modules/gcp-networks/outputs.tf
@@ -1,5 +1,5 @@
-# resource "google_compute_network" "this" {
-#   name                    = var.name
+#resource "google_compute_network" "this" {
+#  name                    = var.name
 #   description             = var.description
 #   auto_create_subnetworks = var.auto_create_subnetworks
 #   routing_mode            = var.routing_mode
